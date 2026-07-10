9b6145f605d65ecee67030f595a263f5c96f0bc7
enod12 <enod12@gmail.com>
2026-07-10 21:55:03 +0300

    Добавлен файл lesson_2_task_3.py

--- lesson_02/lesson_2_task_3.py
+++ lesson_02/lesson_2_task_3.py
@@ -1,10 +1,14 @@
 import math
 
+
 def square(side):
     area = side * side
     if not isinstance(side, int):
         return math.ceil(area)
     return area
 
-print (square(6))
-print (square(6.5))
+
+print(square(6))
+
+
+print(square(6.5))
