db30c7609f3d4cfc41aa32aea138eba2aedfac5d
enod12 <enod12@gmail.com>
2026-07-10 21:52:25 +0300

    Добавлен файл lesson_2_task_3.py

---
+++ lesson_02/lesson_2_task_3.py
@@ -0,0 +1,10 @@
+import math
+
+def square(side):
+    area = side * side
+    if not isinstance(side, int):
+        return math.ceil(area)
+    return area
+
+print (square(6))
+print (square(6.5))
