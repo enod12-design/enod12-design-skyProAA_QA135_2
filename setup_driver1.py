b2a575ac4f1dd184d15e2ca4fb500e9e7ce70a46
enod12 <enod12@gmail.com>
2026-07-31 11:54:46 +0300

    Добавлена папка "lesson_05"

--- setup_driver1.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-
-# Открываем страницу
-driver.get("https://www.google.com")
